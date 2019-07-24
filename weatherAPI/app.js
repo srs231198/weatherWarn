c6e6c6fe1afaac0d46ce4fb86d64b4cc3ebcb749
Shreyas Raj Singh <srs231198@gmail.com>
2019-07-24 06:28:14 +0530

    first commit
    laid down the basic structure of the program!

---
+++ weatherAPI/app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+
+var url = "https://www.metaweather.com/api/location/2388929/";
+
+request(url, function(error, response, body) {
+    if(!error && response.statusCode == 200){
+        var data = JSON.parse(body);
+        console.log(data["consolidated_weather"][1]["weather_state_name"]);
+    }
+});
+
+app.listen(3000, function(){
+    console.log("Weather App has started!!!");
+});

802c14fcd661488aa43b3714349749b4d75e04bd
Shreyas Raj Singh <srs231198@gmail.com>
2019-07-24 17:42:08 +0530

    API working
    Got the metaweather API up and running, now need to standardize output!

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # weatherWarn
-Notification system using metaweather API that warns users of any sudden changes in weather!
+Notification system using metaweather API that informs users of the details of the weather in the area entered!!
 Built using Node.js and Express.js along with ejs!
--- weatherAPI/app.js
+++ weatherAPI/app.js
@@ -1,20 +1,48 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+var bodyParser = require("body-parser");
+
 app.set("view engine", "ejs");
 
-// var url = "https://www.metaweather.com/api/location/2388929/";
-
-// request(url, function(error, response, body) {
-//     if(!error && response.statusCode == 200){
-//         var data = JSON.parse(body);
-//         console.log(data["consolidated_weather"][1]["weather_state_name"]);
-//     }
-// });
+app.use(express.static(__dirname + '/public'));
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+// app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
+
+app.get("/results", function(req, res) {
+    var url = "https://www.metaweather.com/api/location/search/?query=" + req.query.user.location;
+    console.log(url);
+    
+    var locationIdentify = function(){
+        request(url, function(error, response, body) {
+            if(!error && response.statusCode == 200){
+                var data = JSON.parse(body);
+                weatherIdentify(data);
+            } else {
+                console.log(error);
+            }
+        });
+    }
+    var weatherIdentify = function(data) {
+        var url = "https://www.metaweather.com/api/location/"+ data[0].woeid + "/";
+        request(url, function(error, response, body) {
+            if(!error && response.statusCode == 200){
+                var data = JSON.parse(body);
+                console.log(data["consolidated_weather"][0]["weather_state_name"]);
+                res.render("results", {data: data});
+            } else {
+                console.log(error);
+            }
+        });
+    }
+    locationIdentify();
+})
 
 app.listen(3000, function(){
     console.log("Weather App has started!!!");

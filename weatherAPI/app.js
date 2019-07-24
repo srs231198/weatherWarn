7686271cb6c327e2f503c007766940f9b0e91079
Shreyas Raj Singh <srs231198@gmail.com>
2019-07-24 06:40:51 +0530

    landing page setup
    Made the landing page's basic structure

--- weatherAPI/app.js
+++ weatherAPI/app.js
@@ -3,13 +3,17 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
-var url = "https://www.metaweather.com/api/location/2388929/";
+// var url = "https://www.metaweather.com/api/location/2388929/";
 
-request(url, function(error, response, body) {
-    if(!error && response.statusCode == 200){
-        var data = JSON.parse(body);
-        console.log(data["consolidated_weather"][1]["weather_state_name"]);
-    }
+// request(url, function(error, response, body) {
+//     if(!error && response.statusCode == 200){
+//         var data = JSON.parse(body);
+//         console.log(data["consolidated_weather"][1]["weather_state_name"]);
+//     }
+// });
+
+app.get("/", function(req, res) {
+    res.render("landing");
 });
 
 app.listen(3000, function(){
